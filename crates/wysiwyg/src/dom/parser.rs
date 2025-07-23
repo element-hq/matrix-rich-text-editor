e11808145025dc6725f4ec42182c66fa65255c0a
David Langley <davidl@element.io>
2025-07-23 19:14:41 +0100

    Re use the same html chunk in our constants. Add better docs.

--- crates/wysiwyg/src/dom/parser.rs
+++ crates/wysiwyg/src/dom/parser.rs
@@ -50,3 +50,8 @@ use sys::*;
 
 pub use parse::parse;
 pub use parse::parse_from_source;
+
+#[cfg(test)]
+pub use parse::GOOGLE_DOC_HTML_PASTEBOARD;
+#[cfg(test)]
+pub use parse::MS_DOC_HTML_PASTEBOARD;
