cb9fb0638c37c650294ea7a0df1bfb9f2cbda228
Benoit Marty <benoit@matrix.org>
2026-03-11 11:51:34 +0100

    Fix name of the iOS destination

--- platforms/ios/example/ios-test-coverage.sh
+++ platforms/ios/example/ios-test-coverage.sh
@@ -6,7 +6,7 @@ xcodebuild \
   -project Wysiwyg.xcodeproj \
   -scheme WysiwygComposerTests \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=Snapshots,OS=26.1' \
+  -destination 'platform=iOS Simulator,name=iPhone 17,OS=26.1' \
   -derivedDataPath ./DerivedData \
   -resultBundlePath tests.xcresult \
   -enableCodeCoverage YES \
--- platforms/ios/example/ios-ui-test-coverage.sh
+++ platforms/ios/example/ios-ui-test-coverage.sh
@@ -6,7 +6,7 @@ xcodebuild \
   -project Wysiwyg.xcodeproj \
   -scheme Wysiwyg \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=Snapshots,OS=26.1' \
+  -destination 'platform=iOS Simulator,name=iPhone 17,OS=26.1' \
   -derivedDataPath ./DerivedData \
   -resultBundlePath ui-tests.xcresult \
   -enableCodeCoverage YES \
