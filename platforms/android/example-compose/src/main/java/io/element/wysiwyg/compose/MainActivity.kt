1d2026ddaab6c0180f76d4ac8384e91e5b708fd2
Benoit Marty <benoit.marty@gmail.com>
2026-04-03 18:12:02 +0200

    Remove dependency on material in the library. (#105)
    * Remove dependency on com.google.android.material:material from the library.
    
    * Example Compose: fix layout issue.
    
    * Remove other unused dependency.
    
    * Remove unused dependencies from the compose library
    
    * Fix android test compilation issue.

--- platforms/android/example-compose/src/main/java/io/element/wysiwyg/compose/MainActivity.kt
+++ platforms/android/example-compose/src/main/java/io/element/wysiwyg/compose/MainActivity.kt
@@ -12,7 +12,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,6 +25,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
@@ -58,6 +61,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val mentionDisplayHandler = DefaultMentionDisplayHandler()
         val mentionDetector = if (window.decorView.isInEditMode) null else newMentionDetector()
         setContent {
@@ -107,11 +111,15 @@ class MainActivity : ComponentActivity() {
                         },
                         onDismissRequest = { linkDialogAction = null })
                 }
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+                ) { paddingValues ->
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues),
                         verticalArrangement = Arrangement.SpaceBetween
                     ) {
                         var isTyping by remember { mutableStateOf(false) }
